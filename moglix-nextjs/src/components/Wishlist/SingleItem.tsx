c6a74a1505223ccfe59b2f5a9c5e7c92824255e6
sonu <sonu@woodenstreet.com>
2026-05-05 21:42:06 +0530

    wishlist

--- moglix-nextjs/src/components/Wishlist/SingleItem.tsx
+++ moglix-nextjs/src/components/Wishlist/SingleItem.tsx
@@ -29,7 +29,7 @@ const SingleItem = ({ item }) => {
         <button
           onClick={() => handleRemoveFromWishlist()}
           aria-label="button for remove product from wishlist"
-          className="flex items-center justify-center rounded-lg max-w-[38px] w-full h-9.5 bg-gray-2 border border-gray-3 ease-out duration-200 hover:bg-red-light-6 hover:border-red-light-4 hover:text-red"
+          className=" a flex items-center justify-center rounded-lg max-w-[38px] w-full h-9.5 bg-gray-2 border border-gray-3 ease-out duration-200 hover:bg-red-light-6 hover:border-red-light-4 hover:text-red"
         >
           <svg
             className="fill-current"
