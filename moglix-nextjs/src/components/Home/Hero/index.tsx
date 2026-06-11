e9f97601c624f6e0f07b591f58e7fbb7de5a20f4
nayan soni <69868558+nayansoni1402@users.noreply.github.com>
2026-06-11 23:05:21 +0530

    Added

--- moglix-nextjs/src/components/Home/Hero/index.tsx
+++ moglix-nextjs/src/components/Home/Hero/index.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 const Hero = () => {
   return (
-    <section className="w-full bg-[#F4F5F9] pt-4 pb-6">
+    <section className="w-full bg-[#F4F5F9] pb-6">
       <div className="max-w-[1300px] mx-auto px-4 sm:px-8 xl:px-0">
         <Link href="/category/power-tools" className="block w-full rounded-xl overflow-hidden shadow-md group">
           <Image
